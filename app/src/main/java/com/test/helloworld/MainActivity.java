30ab630c38580daa2bb3fcf4003031a135d9e550
menglingyun <menglingyun0421@rayootech.com>
2018-04-08 16:44:52 +0800

    增加一个TEXT VIEW

--- app/src/main/java/com/test/helloworld/MainActivity.java
+++ app/src/main/java/com/test/helloworld/MainActivity.java
@@ -2,12 +2,25 @@ package com.test.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initData();
+    }
+
+    private void initView(){
+        tv_main=findViewById(R.id.tv_main);
+    }
+
+    private void initData(){
+        tv_main.setText("上传成功了");
     }
 }
